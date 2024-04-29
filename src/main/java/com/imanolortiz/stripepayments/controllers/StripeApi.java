4056dd2cbd8ca8c5f92de4a96b27afcd4f8f066d
Imanol <imanolof29@gmail.com>
2024-04-29 10:12:32 +0200

    stripe endpoint fix

--- src/main/java/com/imanolortiz/stripepayments/controllers/StripeApi.java
+++ src/main/java/com/imanolortiz/stripepayments/controllers/StripeApi.java
@@ -5,12 +5,9 @@ import com.imanolortiz.stripepayments.commons.dtos.CheckoutRequest;
 import com.imanolortiz.stripepayments.commons.dtos.CheckoutResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController(ApiPathConstants.V1_ROUTE + ApiPathConstants.STRIPE_ROUTE)
+@RequestMapping(ApiPathConstants.V1_ROUTE + ApiPathConstants.STRIPE_ROUTE)
 public interface StripeApi {
 
     @PostMapping(value = "/webhook")
